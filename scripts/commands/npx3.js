350dad2eaa2b977ff5bcedae093a78f75dcf5871
DAVIL KING <148178117+evaldavilhkr@users.noreply.github.com>
2024-08-24 18:05:46 +0600

    Update npx3.js

--- scripts/commands/npx3.js
+++ scripts/commands/npx3.js
@@ -17,6 +17,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
   const axios = require('axios')
+  const NAYAN = ['https://i.imgur.com/LLucP15.mp4','https://i.imgur.com/DEBRSER.mp4']
     var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
 const media = (
     await axios.get(
@@ -25,13 +26,13 @@ const media = (
     )
   ).data;
 
-	if (body.indexOf("🥰")==0 || body.indexOf("🤩")==0 || body.indexOf("😍")==0 || body.indexOf("🥰")==0 || body.indexOf("🐸")==0 || body.indexOf("💔")==0 || body.indexOf("😈")==0 || body.indexOf("😉")==0 || body.indexOf("😜")==0 || body.indexOf("🥹")==0) {
+	if (body.indexOf("🥰")==0 || body.indexOf("🤩")==0 || body.indexOf("😍")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
 		var msg = {
 				body: "🖤🥀",
-				body: "ইমোজি দাও কেন শুধু শুধু😒😒"
+				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😈", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
 		}
 	},
 	start: function({ nayan }) {
